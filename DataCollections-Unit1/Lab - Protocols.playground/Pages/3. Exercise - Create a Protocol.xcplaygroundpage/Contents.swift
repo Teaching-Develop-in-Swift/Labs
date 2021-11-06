b1a0af419ab5cb2caa561e6f06545c99b1d53f60
riccardorizzo94 <rizzoriccardo.1994@gmail.com>
2021-11-06 12:32:41 +0100

    Unit 1 - Page 3

--- DataCollections-Unit1/Lab - Protocols.playground/Pages/3. Exercise - Create a Protocol.xcplaygroundpage/Contents.swift
+++ DataCollections-Unit1/Lab - Protocols.playground/Pages/3. Exercise - Create a Protocol.xcplaygroundpage/Contents.swift
@@ -3,14 +3,37 @@
  
  Create a protocol called `Vehicle` with two requirements: a nonsettable `numberOfWheels` property of type `Int`, and a function called `drive()`.
  */
-
+protocol Veichle {
+    var numberOfWheels: Int { get }
+    func drive()
+}
 
 //:  Define a `Car` struct that implements the `Vehicle` protocol. `numberOfWheels` should return a value of 4, and `drive()` should print "Vroom, vroom!" Create an instance of `Car`, print its number of wheels, then call `drive()`.
+struct Car: Veichle {
+    
+    var numberOfWheels: Int { return 4 }
+    
+    func drive() {
+        print("Vroom, vroom!")
+    }
+}
 
-
+let car = Car()
+print("Number of wheels: \(car.numberOfWheels)")
+car.drive()
 //:  Define a `Bike` struct that implements the `Vehicle` protocol. `numberOfWheels` should return a value of 2, and `drive()` should print "Begin pedaling!". Create an instance of `Bike`, print its number of wheels, then call `drive()`.
+struct Bike: Veichle {
+    
+    var numberOfWheels: Int { return 2 }
+    
+    func drive() {
+        print("Begin pedaling!")
+    }
+}
 
-
+let bike = Bike()
+print("Number of wheels: \(bike.numberOfWheels)")
+bike.drive()
 /*:
 [Previous](@previous)  |  page 3 of 5  |  [Next: App Exercise - Similar Workouts](@next)
  */
