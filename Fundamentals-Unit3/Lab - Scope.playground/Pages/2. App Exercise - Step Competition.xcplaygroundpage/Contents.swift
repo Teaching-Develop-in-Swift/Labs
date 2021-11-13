3097f5050b9b3486931cd5e6e01e147db0c8824f
Simon West <43855704+simonwestuk@users.noreply.github.com>
2021-11-13 10:28:13 +0000

    Added Scope.

--- Fundamentals-Unit3/Lab - Scope.playground/Pages/2. App Exercise - Step Competition.xcplaygroundpage/Contents.swift
+++ Fundamentals-Unit3/Lab - Scope.playground/Pages/2. App Exercise - Step Competition.xcplaygroundpage/Contents.swift
@@ -8,6 +8,22 @@
 struct User {
     var name: String
     var stepsToday: Int
+    
+    init(name: String, stepsToday: Int) {
+        
+        self.name = name
+        self.stepsToday = stepsToday
+    }
+
+    init?(name: String?, stepsToday: Int?) {
+        
+        guard let name = name,
+              let stepsToday = stepsToday
+        else {return nil}
+
+        self.name = name
+        self.stepsToday = stepsToday
+    }
 }
 
 let stepMaster = User(name: "StepMaster", stepsToday: 8394)
@@ -24,8 +40,8 @@ func getWinner(competitors: [User]) -> User? {
     var topCompetitor: User?
 
     for competitor in competitors {
-        if let topCompetitor = topCompetitor {
-            if competitor.stepsToday > topCompetitor.stepsToday {
+        if let top = topCompetitor {
+            if competitor.stepsToday > top.stepsToday {
                 topCompetitor = competitor
             }
         } else {
@@ -36,12 +52,18 @@ func getWinner(competitors: [User]) -> User? {
 }
 
 
+let winner = getWinner(competitors: competitors)
+print(winner!.name)
+
+
+
 //:  Write a memberwise initializer inside the `User` struct above that uses variable shadowing for naming the parameters of the initializer.
 
-
+//Above
 //:  Now write a failable initializer inside the `User` struct above that takes parameters `name` and `stepsToday` as an optional `String` and `Int`, respectively. The initializer should return `nil` if either of the parameters are `nil`. Use variable shadowing when unwrapping the two parameters.
-
-
+User(name: "Simon", stepsToday: 1000)
+User(name: nil, stepsToday: 1000)
+User(name: "Simon", stepsToday: nil)
 /*:
  _Copyright © 2021 Apple Inc._
 
