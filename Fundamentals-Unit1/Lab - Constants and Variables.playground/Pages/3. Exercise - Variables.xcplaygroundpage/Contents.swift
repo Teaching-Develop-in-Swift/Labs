a32534584c154a6ae0bd88b18e0aab28b4d4a1bc
Api Phoom <apiphoom23@gmail.com>
2023-02-11 17:14:33 +0700

    update : labs

--- Fundamentals-Unit1/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ Fundamentals-Unit1/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -4,12 +4,18 @@
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
 
+var schooling = 6
+print("Years of school completed: \(schooling)")
 
 //:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+
+schooling += 1
+print("Years of school completed: \(schooling)")
 
 
 //:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
 
+print("The value of a variable declared with 'var' can be changed, whereas the value of a constant declared with 'let' cannot be changed.")
 
 /*:
 [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
--- Fundamentals-Unit1/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Fundamentals-Unit1/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -4,15 +4,28 @@
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
 
+var firstDecimal: Double = 3.14159
+var secondDecimal: Float = 2.71828
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+var trueOrFalse: Bool = true
+firstDecimal = trueOrFalse
 
+print("Won't compile because a boolean value and a decimal value are different types")
+print("firstDecimal is of type \(type(of: firstDecimal))")
+print("secondDecimal is of type \(type(of: secondDecimal))")
 
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
-
+var stringValue: String = "Hello"
+firstDecimal = stringValue
+print("Won't compile because a string value and a decimal value are different types")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
+var wholeNumber: Int = 42
+print("Cannot assign a variable of type \(type(of: trueOrFalse)) to firstDecimal of type \(type(of: firstDecimal))")
+print("Cannot assign a variable of type \(type(of: stringValue)) to firstDecimal of type \(type(of: firstDecimal))")
+print("Cannot assign a variable of type \(type(of: wholeNumber)) to firstDecimal of type \(type(of: firstDecimal)) without explicit type conversion")
 
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
